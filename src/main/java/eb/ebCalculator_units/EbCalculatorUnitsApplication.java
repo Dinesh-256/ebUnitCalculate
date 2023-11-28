e2584322fa76656f3901695598d4bcbf802cfd76
Dinesh-256 <mddinesh108@gmail.com>
2023-11-28 20:53:32 +0530

    Commit

---
+++ src/main/java/eb/ebCalculator_units/EbCalculatorUnitsApplication.java
@@ -0,0 +1,13 @@
+package eb.ebCalculator_units;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbCalculatorUnitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbCalculatorUnitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eb/ebCalculator_units/EbCalculatorUnitsApplicationTests.java
@@ -0,0 +1,13 @@
+package eb.ebCalculator_units;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbCalculatorUnitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
